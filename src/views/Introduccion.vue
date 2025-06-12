0a6dea1d001a6e9afc87c752d8970b9a7e985f47
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-06-12 13:03:33 -0500

    Se carga video introducción, XD ajustado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,8 +28,10 @@
         .row.bgmorado
           p.py-3.px-5 En definitiva, la energía eléctrica es indispensable en nuestra vida diaria y en el progreso de la sociedad. No obstante, para aprovecharla de manera eficiente y segura, es fundamental adoptar medidas de precaución que protejan tanto nuestra integridad física como los bienes materiales. Una red eléctrica bien diseñada y utilizada no solo optimiza el rendimiento de los equipos, sino que también reduce significativamente los riesgos eléctricos.
 
-        
-
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DTveW204nFc?si=HfsVOKaLP1DJzieP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Seguridad e implementación de las instalaciones eléctricas.
 
 
 
