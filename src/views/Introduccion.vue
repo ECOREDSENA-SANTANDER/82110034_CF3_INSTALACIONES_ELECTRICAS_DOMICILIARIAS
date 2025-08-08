5d1967573e69e494f70cbf5df38b79d2dc697f7e
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-08-08 13:07:49 -0500

    avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -30,7 +30,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/DTveW204nFc?si=HfsVOKaLP1DJzieP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/MZXmfSbPato" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Seguridad e implementación de las instalaciones eléctricas.
 
 
